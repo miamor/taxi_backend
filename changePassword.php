f2f032c8c484ff28df469727dcfb3155293f001c
miamor <miamorwest@gmail.com>
2017-09-03 20:29:54 +0700

    Change password

--- changePassword.php
+++ changePassword.php
@@ -4,14 +4,14 @@ include 'config.php';
     include 'objects/taxi.php';
     $taxi = new Taxi();
 
-    $taxi->id = isset($_POST['taxiid']) ? $_POST['taxiid'] : 1;
-    $taxi->password = isset($_POST['password']) ? $_POST['password'] : 'abc123';
+    $taxi->id = isset($_POST['taxiid']) ? $_POST['taxiid'] : null;
+    $taxi->password = isset($_POST['password']) ? $_POST['password'] : null;
 
 //    echo $_POST['username'];
 
     if ($taxi->id && $taxi->password) {
         $do = $taxi->updatePassword();
         $data = array('newPassword'=>$taxi->passwordHash);
-        echo ($do ? $data : 0);
+        echo ($do ? json_encode($data, JSON_UNESCAPED_UNICODE) : 0);
     } else echo -1;
 //} else echo -2;
